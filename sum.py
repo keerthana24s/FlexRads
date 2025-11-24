48bebb7df0a1f5ba2100ccacbe48e1c71a8aa1cb
Keerthana <keerthana.bhartidiagnostics@gmail.com>
2025-11-24 16:32:33 +0530

    diff added

--- sum.py
+++ sum.py
@@ -5,5 +5,11 @@ num2 = 10
 # Calculate the sum
 sum_result = num1 + num2
 
+#calculate the difference
+diff = num2-num1
+
 # Print the result
 print(f"The sum of {num1} and {num2} is: {sum_result}")
+print(f"The sum of {num1} and {num2} is: {diff}")
+
+
